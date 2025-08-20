7eba419a477dc44cf9b14b256b3740481ac49eff
pforu <potterhead0402@naver.com>
2025-08-20 14:02:07 +0900

    row_screen commit

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -3,6 +3,8 @@ import 'package:smwu_202508/screen/container/container_screen.dart';
 import 'package:smwu_202508/screen/container/container_practice_screen.dart';
 import 'package:smwu_202508/screen/column/column_screen.dart';
 import 'package:smwu_202508/screen/column/column_practice_screen.dart';
+import 'package:smwu_202508/screen/row/row_practice_screen.dart';
+import 'package:smwu_202508/screen/row/row_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -72,6 +74,36 @@ class ButtonScreen extends StatelessWidget {
                 );
               },
               child: Text("Column 실습"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                //눌렸을 때 중괄호 진입
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RowScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Row"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                //눌렸을 때 중괄호 진입
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RowPracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Row 실습"),
             ),
           ],
         ),
