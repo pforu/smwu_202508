26006f89b943895ba0f13b204073bd103c584dc1
pforu <potterhead0402@naver.com>
2025-08-25 15:07:50 +0900

    inner state 설명

--- lib/screen/stateful/inner_state.dart
+++ lib/screen/stateful/inner_state.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 
 class InnerState extends StatefulWidget {
-  const InnerState({required this.count, super.key});
+  const InnerState({required this.count, super.key}); // 1. 호출될 때 count를 받음
 
-  final int count;
+  final int count; //count를 가지기 때문
 
   @override
-  State<InnerState> createState() => _InnerStateState();
+  State<InnerState> createState() => _InnerStateState(); // 2. 상태는 얘(_)가 관리함
 }
 
 class _InnerStateState extends State<InnerState> {
-  int innerCount = 0;
+  int innerCount = 0; // 3. 이쪽에서는 변수 따로 만듦
 
   @override
   void initState() {
     //stateful 위젯이 최초 생성될 때 한 번만 호출
     // TODO: implement initState
-    super.initState();
-    innerCount = widget.count;
+    super.initState(); //클래스명에 있는 State<InnerState>(내부클래스)를 상속
+    innerCount = widget.count; // 4. 받아온 count로 초기화
   }
 
   @override
